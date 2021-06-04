328891f4f7f127f520a2df220ef6e6910a628f5f
sammiefrog <sammiefrog@live.com>
2021-06-04 10:58:33 -0500

    printing all results

--- src/com/example/MSGrades/MSGrades.java
+++ src/com/example/MSGrades/MSGrades.java
@@ -37,35 +37,51 @@ public class MSGrades {
 
     public void printStudents(){
         // loop thru the students ArrayList with a For Each loop
+        for(String student : students){
+            System.out.println(student);
+        }
             // print each students name on a new line as output
     }
 
     // Make a method that prints an individual students grades
     // It will be public, not have a return value,
     // and have one int type parameter that is a students index
-
-    // Inside the method:
+    public void printStudentGrades(int studentIndex){
+        // Inside the method:
         // Print the students name by using the index variable to index the students array
+        System.out.println(students.get(studentIndex));
         // Print a concatenated string of say "Math: " + and the corresponding grade
-            // from that ArrayList of grades by index
+        System.out.println("Math: " + mathGrades.get(studentIndex));
+        System.out.println("Science: " + scienceGrades.get(studentIndex));
+        System.out.println("English: " + englishGrades.get(studentIndex));
+        // from that ArrayList of grades by index
         // Do this for the next two subjects
         // Total of four output statements
+    }
+
+
 
     public static void main(String[] args) {
 
         // create a new instance of MSGrades class and store in a variable
             // (anything like msGrade8)
+        MSGrades msGrades8 = new MSGrades();
         // look up how to print an empty new line
+        System.out.println();
             // for readability
         
         System.out.println("Middle School Roster");
         System.out.println("====================");
 
         // invoke or run the printStudents method on our class instance
-
+        msGrades8.printStudents();
         // Print another empty line if desired for readability
-
+        System.out.println();
         // Use a for loop to loop thru the given teachers array on the class instance
+        for(int i = 0; i < msGrades8.teachers.length; i++){
+            String teacher = "Teacher: " + msGrades8.teachers[i];
+            System.out.println(teacher);
+        }
         // inside the loop
             // create a new variable that stores the result of "Teacher: " +
                 // teachers name from the array using the counter variable
@@ -74,6 +90,10 @@ public class MSGrades {
         System.out.println("\nStudent Grades:\n");
 
         // Use a for loop to loop thru students ArrayList on the class instance
+        for(int i = 0; i < msGrades8.students.size(); i++){
+            msGrades8.printStudentGrades(i);
+            System.out.println();
+        }
         // inside the loop
             // pass the counter var as an index to the printStudentGrades method
             // to print each students info
